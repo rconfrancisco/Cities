93bb998d8eaf16928eff0a49c429831f3db612b7
Bob Confrancisco <rconfrancisco@gmail.com>
2017-06-26 22:33:56 -0400

    Change from recursive to interative.

--- Cities.C
+++ Cities.C
@@ -10,30 +10,26 @@
 #include <memory>
 
 using CitiesSet_t         = std::unordered_set<std::string>;
-using CitiesSetItr_t      = CitiesSet_t::iterator;
-using CitiesSetConstItr_t = CitiesSet_t::const_iterator;
 
 struct Node_t {
   bool        visited;
   std::shared_ptr<CitiesSet_t> connectedCities;
   Node_t() : visited(false) , connectedCities(std::make_shared<CitiesSet_t>()) {};
-
 };
 
 using CitiesMap_t         = std::unordered_map<std::string, Node_t>;
 using CitiesMapItr_t      = CitiesMap_t::iterator;
-using CitiesMapConstItr_t = CitiesMap_t::const_iterator;
 using NodeQueue_t         = std::queue<Node_t>;
 
 void insertCity(CitiesMap_t&       city_map, 
 		const std::string& city1, 
 		const std::string& city2){
 
-  CitiesMapItr_t c_itr = city_map.find(city1);
+  auto c_itr = city_map.find(city1);
   if(c_itr == city_map.end() ) {
     Node_t      node;
-    std::pair<CitiesMapItr_t, bool> rv;
-    rv = city_map.insert(std::pair<std::string, Node_t>(city1, node));
+    std::pair<CitiesMapItr_t, bool> rv = 
+      city_map.insert(std::pair<std::string, Node_t>(city1, node));
     c_itr = rv.first;
   }
   if(c_itr != city_map.end() ) {
@@ -45,64 +41,53 @@ void insertCity(CitiesMap_t&       city_map,
 bool checkCitiesExist(const CitiesMap_t& city_map,
 		      const std::string& city1, 
 		      const std::string& city2) {
-  CitiesMap_t::const_iterator c_itr1 = city_map.find(city1);
-  CitiesMap_t::const_iterator c_itr2 = city_map.find(city2);
-  if((c_itr1 == city_map.end()) ||
-     (c_itr2 == city_map.end())) {
-    return false;
+
+  auto c_itr1 = city_map.find(city1);
+  auto c_itr2 = city_map.find(city2);
+  auto c_end  = city_map.end();
+  bool rv=false;
+  if((c_itr1 != c_end) && 
+     (c_itr2 != c_end) &&
+     (!c_itr1->second.connectedCities.get()->empty()) &&
+     (!c_itr2->second.connectedCities.get()->empty())) {
+      rv = true;
   }
-  if((c_itr1->second.connectedCities.get()->empty()) ||
-     (c_itr2->second.connectedCities.get()->empty())) return false;
-  
-  return true;
+  return rv;
 }
 
 bool findCity(CitiesMap_t&       city_map, 
-	      NodeQueue_t&       node_queue,
-	      const std::string& city2) {
+	      const std::string& begin_city,
+	      const std::string& city) {
 
-  if(node_queue.empty()) return false;
-  //Pop 1 element off queue and process it.
-  Node_t node = node_queue.front();
-  node_queue.pop();
-
-  CitiesSetItr_t      cs_itr=node.connectedCities.get()->begin();
-  CitiesSetConstItr_t cs_end=node.connectedCities.get()->end();
-  CitiesMapItr_t      cm_itr=city_map.begin();
-  CitiesMapConstItr_t cm_end=city_map.end();
-  bool rv=false;
-  while(cs_itr != cs_end) {
-    if(*cs_itr == city2) {
-      rv = true;
-      break;
-    }
-    cm_itr = city_map.find(*cs_itr);
-    if(cm_itr != cm_end) {
-      if(cm_itr->second.visited == false) {
-	cm_itr->second.visited = true;
-	node_queue.push(cm_itr->second);
+  NodeQueue_t node_queue;
+  auto c_itr = city_map.find(begin_city);
+  if(c_itr != city_map.end() ) {
+    c_itr->second.visited = true;
+    node_queue.push(c_itr->second);
+  }
+  while(!node_queue.empty()) {
+  
+    //Pop 1 element off queue and process it.
+    Node_t node = node_queue.front();
+    node_queue.pop();
+    auto cs_itr=node.connectedCities.get()->begin();
+    auto cs_end=node.connectedCities.get()->end();
+    while(cs_itr != cs_end) {
+      if(*cs_itr == city) {
+	return true;
       }
+      auto cm_itr = city_map.find(*cs_itr);
+      if(cm_itr != city_map.end()) {
+	if(cm_itr->second.visited == false) {
+	  cm_itr->second.visited = true;
+	  node_queue.push(cm_itr->second);
+	}
+      }
+      ++cs_itr;
     }
-    ++cs_itr;
   }
-
-  if(rv==false) {
-    rv = findCity(city_map, node_queue, city2);
-  }
-
-  return rv;
+  return false;
 };
-
-void seedQueue(const CitiesMap_t& city_map, 
-	             NodeQueue_t& node_queue, 
-	       const std::string& begin_city) {
-
-    CitiesMapConstItr_t c_itr = city_map.find(begin_city);
-    if(c_itr != city_map.end() ) {
-      node_queue.push(c_itr->second);
-    }
-    return;
-}
 
 int main(int argc, char* argv[]) {
 
@@ -129,11 +114,9 @@ int main(int argc, char* argv[]) {
 
   bool connected = false;
   if(checkCitiesExist(cityMap, ocity1, ocity2)) {
-    NodeQueue_t node_queue;
-    seedQueue(cityMap, node_queue, ocity1);
-    connected = findCity(cityMap, node_queue, ocity2);
+    connected = findCity(cityMap, ocity1, ocity2);
   }
-
-  std::cout << ocity1 << ((connected) ? " is" : " is not") << " connected to " << ocity2 << std::endl;
+  std::cout << ocity1 << " is" << ((connected) ? "" : " not") << " connected to " 
+	    << ocity2 << std::endl;
   return 0;
 }
